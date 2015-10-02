9092a94d480db0238507be23377f751bef7b6e8f
Yakov_work <Proglotina849@mail.ru>
2015-10-02 12:45:10 +0300

    fix task3 in homeWork1 L2_tasks

--- Proga/src/L2_tasks/Task3.java
+++ Proga/src/L2_tasks/Task3.java
@@ -5,8 +5,8 @@ import java.util.regex.Pattern;
 
 public class Task3 {
     public static void main(String[] args) {
-        Pattern pattern1 = Pattern.compile("^(((https?)\\:\\/\\/)(www\\.)?(((([a-z0-9-]))+)\\.){1,2}([a-z]+)(\\/?([a-z0-9-/])*))");
-        Matcher url = pattern1.matcher("http://a.google.com/dsf/sdfds/sdfhjsd");
+        Pattern pattern1 = Pattern.compile("^(((https?)\\:\\/\\/)(((([a-z0-9-]))+)\\.)+([a-z]+)(\\/?([a-z0-9-/])*))");
+        Matcher url = pattern1.matcher("http://www.a.b.c.google.com");
 
         System.out.println(url.matches());
     }
